30259615cf41ba7fd94cbe6e591ef5bba19c44e1
AnthonyPillow <anthonypillow18@gmail.com>
2020-01-18 23:24:20 -0800

    tried troubleshooting the js to no avail. Note to self need to commit more often.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const form = document.getElementById('generatorForm')
+var form = document.getElementById('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
 
 const includeUpperCaseElement = document.getElementById('useUpperCase')
 const includeNumbersElement = document.getElementById('useNumbers')
@@ -7,37 +8,46 @@ const characterAmountElement = document.getElementById('charNumber')
 
 
 
-form.addEventListener('submit', e => {
+document.getElementById('passwordGeneratorForm').addEventListener('submit', e => {
     e.preventDefault()
     const characterAmount = characterAmountElement.value
-    const includeUpperCase = includeUpperCase.checked
+    const includeUpperCase = includeUpperCaseElement.checked
     const includeNumbers = includeNumbersElement.checked
     const includeSymbols = includeSymbolsElement.checked
     const password = generatePassword(characterAmount, includeUpperCase, includeNumbers, includeSymbols)
-
+    passwordDisplay.innertext = password
 })
 
-const LOWERCASE_CHAR_CODES = arrayFromLowtoHigh(65, 90)
-const UPPERCASE_CHAR_CODES = arrayFromLowtoHigh(97, 112)
+const UPPERCASE_CHAR_CODES = arrayFromLowtoHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowtoHigh(97, 112)
 const NUMBER_CHAR_CODES = arrayFromLowtoHigh(48, 57)
-const SYMBOL_CHAR_CODES = arrayFromLowtoHigh(33, 47)
-    .concat(
-        arrayFromLowtoHigh(58, 64)
-            .concat(
-                arrayFromLowtoHigh(91, 96)
-                    .concat(
-                        arrayFromLowtoHigh(123, 126)
-                    )
-            )
+const SYMBOL_CHAR_CODES = arrayFromLowtoHigh(33, 47).concat(
+    arrayFromLowtoHigh(58, 64)
+    ).concat(
+    arrayFromLowtoHigh(91, 96)
+    ).concat(
+    arrayFromLowtoHigh(123, 126)
     )
 
-function generatePassword(characterAmount, includeNumbers, includeSymbols, characterAmount) {
-
+function generatePassword(characterAmount, includeNumbers, includeSymbols, includeUpperCase) {
+    let charCodes = LOWERCASE_CHAR_CODES
+    if(includeUpperCase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+    if(includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+    if(includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+    
+    const passwordCharacters = []
+    for (let i = 0; i < characterAmount; i++) {
+        const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+        passwordCharacters.push(String.fromCharCode(characterCode))
+    }
+    return passwordCharacters.join('')
 }
 
 function arrayFromLowtoHigh(low, high) {
     const array = []
-    for (var i = 0; i <= high; i++) {
+    for (var i = low; i <= high; i++) {
         array.push(i)
     }
+    return array 
 }
+
